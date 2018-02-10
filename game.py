a0faba49541fa8d2294a1a0a6caaba3d06c7e832
Hackbright Student <no-reply@hackbrightacademy.com>
2018-02-10 12:46:17 -0800

    Working guessing-game

--- game.py
+++ game.py
@@ -1,4 +1,20 @@
 """A number-guessing game."""
 name = raw_input("please enter your name: ")
 print "Welcome to the guessing game {}, have fun" .format(name)
-# Put your code here
+import random
+
+# make counter
+counter = 1
+num = random.randint(1, 100)
+user_guess = None
+while num != user_guess:
+    user_guess = int(raw_input("Please enter your guess: "))
+
+#while num != user_guess:
+    if user_guess < num:
+        print "Your guess is too low, try again"
+    elif user_guess > num:
+        print "Your guess is too high. Try again."
+    else:
+        print "Well done. {}! You found the number in {} tries.".format(name, counter)
+    counter = counter + 1
